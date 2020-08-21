d9a841fba6206ab60ddac51420b567a273b862dc
Ryan Brereton-Canham <ryan@luminsports.com>
2020-08-21 16:56:53 +0930

    feat: ignore unused rest destructured params

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ module.exports = {
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-unused-vars': ["error", { "args": "none" }]
+    '@typescript-eslint/no-unused-vars': ["error", { "args": "none", "ignoreRestSiblings": true }],
     'camelcase': 'off',
     'no-console': ['error', {'allow': ['warn', 'error']}],
     'no-debugger': 'error',

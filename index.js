8648b863e6287fef723e0277ebae305a3ec04ad6
Ryan Brereton-Canham <ryan@luminsports.com>
2020-08-21 13:31:55 +0930

    feat: new eslint config

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 module.exports = {
-  plugins: ['@typescript-eslint'],
   extends: [
     'plugin:vue/recommended',
-    '@vue/standard'
+    '@vue/typescript/recommended'
   ],
+  parserOptions: {
+      ecmaVersion: 2020
+  },
   rules: {
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'camelcase': 'off',
     'no-console': ['error', {'allow': ['warn', 'error']}],
     'no-debugger': 'error',
-    'camelcase': 'off',
-    'vue/singleline-html-element-content-newline': 'off',
-    'vue/max-attributes-per-line': ['error', {
-      singleline: 3,
-      multiline: 1
-    }],
+    'no-undef': 'off',
     'vue/html-closing-bracket-newline': ['error', {
       singleline: 'never',
       multiline: 'never'
     }],
-    'no-undef': 'off',
-    'no-unused-vars': 'off'
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 3,
+      multiline: 1
+    }],
+    'vue/singleline-html-element-content-newline': 'off',
   },
-  parserOptions: {
-    parser: require.resolve('@typescript-eslint/parser')
-  }
 }
 

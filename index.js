cadcdaee2fa541ebe95c9b8b9beecfb0df1c8ebc
Ryan Brereton <ryan@luminsports.com>
2022-04-01 15:00:22 +1030

    feat: consistent function spacing

--- index.js
+++ index.js
@@ -14,16 +14,28 @@ module.exports = {
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { 'args': 'none', 'ignoreRestSiblings': true }],
+    '@typescript-eslint/no-unused-vars': ['error', {'args': 'none', 'ignoreRestSiblings': true}],
     'camelcase': 'off',
     'no-console': ['error', {'allow': ['warn', 'error']}],
     'no-debugger': 'error',
     'no-undef': 'off',
     'no-multi-spaces': ['error'],
-    'space-in-parens': ['error', 'never'],
+    'indent': ['error', 2, {'FunctionExpression': {'parameters': 'first'}}],
+    'computed-property-spacing': ['error', 'never'],
     'object-curly-spacing': ['error', 'always'],
+    'space-before-blocks': ['error', 'always'],
+    'keyword-spacing': ['error', {'before': true, 'after': true}],
+    'arrow-spacing': ['error', {'before': true, 'after': true}],
+    'comma-spacing': ['error', {'before': false, 'after': true}],
+    'array-bracket-spacing': ['error', 'never'],
+    'func-call-spacing': ['error', 'never'],
+    'space-in-parens': ['error', 'never'],
+    'space-infix-ops': ['error'],
+    'space-unary-ops': ['error', {'words': true, 'nonwords': false, 'overrides': { '!': true }}],
     'function-call-argument-newline': ['error', 'consistent'],
     'function-paren-newline': ['error', 'consistent'],
+    'object-curly-newline': ['error', {'consistent': true}],
+    'prefer-const': ['error'],
     'vue/html-closing-bracket-newline': ['error', {
       singleline: 'never',
       multiline: 'never'
@@ -35,5 +47,6 @@ module.exports = {
     'vue/singleline-html-element-content-newline': 'off',
   },
 }
+
 
 

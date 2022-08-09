e68b910109ae79181a4865b754781427daf13fb4
Ryan Brereton <ryan@luminsports.com>
2022-08-09 16:17:27 +0930

    feat: release latest eslint rules

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 module.exports = {
   extends: [
+    'plugin:import/recommended',
     'plugin:vue/recommended',
     '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    project: ['./tsconfig.json'],
+  },
+  settings: {
+    'import/resolver': {
+      'typescript': true,
+      'node': true,
+    }
   },
   rules: {
+    'semi': 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      multiline: { delimiter: 'none' },
+      singleline: { delimiter: 'comma' }
+    }],
     '@typescript-eslint/no-inferrable-types': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-unused-vars': ['error', {'args': 'none', 'ignoreRestSiblings': true}],
+    '@typescript-eslint/consistent-type-exports': ['error', {
+      fixMixedExportsWithInlineTypeSpecifier: true
+    }],
+    '@typescript-eslint/consistent-type-imports': ['error', {
+      prefer: 'type-imports'
+    }],
+    '@typescript-eslint/consistent-indexed-object-style': [
+      'error',
+      'record'
+    ],
+    '@typescript-eslint/consistent-generic-constructors': [
+      'error',
+      'constructor'
+    ],
+    '@typescript-eslint/semi': ['error', 'never'],
     'camelcase': 'off',
     'no-console': ['error', {'allow': ['warn', 'error']}],
     'no-debugger': 'error',
@@ -36,6 +63,7 @@ module.exports = {
     'function-paren-newline': ['error', 'consistent'],
     'object-curly-newline': ['error', {'consistent': true}],
     'prefer-const': ['error'],
+    'import/no-duplicates': ['error'],
     'vue/html-closing-bracket-newline': ['error', {
       singleline: 'never',
       multiline: 'never'
@@ -47,6 +75,3 @@ module.exports = {
     'vue/singleline-html-element-content-newline': 'off',
   },
 }
-
-
-
